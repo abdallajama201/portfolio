f953e86e3023ffa8a9702ee9ac0c4e58498049cc
Abdalla Jama <abdallajama201@gmail.com>
2022-08-16 19:41:14 -0400

    project css and overall coloring

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import TokenOutlinedIcon from '@mui/icons-material/TokenOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import Style from './Header.scss'
 import portrait from '../img/portrait.png'
 
@@ -7,13 +8,16 @@ let links = ["About Me","Portfolio","Resume"];
 export default function footerLinks({currentPage, handlePageChange}) {
   return (
     <div className="column">
-    <div className="row justify-content-center">
+    <div className="row justify-content-around">
       {links.map((link, index) => 
-    <button key={index} href={"#"+link} class={`custom-btn btn-12 col-8 col-lg-2 my-5 ${currentPage === link ? 'active' : 'inactive'}`} onClick={() => handlePageChange(link)}><span>{link}</span><span>{link}</span></button>)}
+    <button key={index} href={"#"+link} class={`custom-btn btn-12 col-8 col-lg-2 my-5 m-1 ${currentPage === link ? 'active' : 'inactive'}`} onClick={() => handlePageChange(link)}><span>{link}</span><span>{link}</span></button>)}
     </div>
     <div className="row justify-content-center">
         <div className='col-8 col-lg-4'>
           <img src={portrait} className='portrait' alt='portrait'></img>
+        </div>
+        <div className='col-8 col-lg-4'>
+            <a href="mailto:abdallajama@gmail.com"><p><EmailOutlinedIcon></EmailOutlinedIcon> abdallajama@gmail.com</p></a>
         </div>
         <TokenOutlinedIcon className='spinner col-12 col-lg-4' sx={{ fontSize: 240 }} />
     </div>
